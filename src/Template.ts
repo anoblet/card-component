f5a60540e676a45d70229f2d33fe4384f9ec55c5
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-02 08:39:21 -0400

    Chore: Save

--- src/Template.ts
+++ src/Template.ts
@@ -3,18 +3,22 @@ import { expand_less, expand_more } from "@anoblet/material-icons";
 
 export default function() {
   return html`
-    <div id="header">
-      <slot name="title"></slot>${this.collapsible
-        ? html`
-            <span id="icon">${this.collapsed ? expand_less : expand_more}</span>
-          `
-        : ""}
-    </div>
-    <div id="body">
-      <slot name="body"></slot>
-    </div>
-    <div id="footer">
-      <slot name="footer"></slot>
+    <div id="card">
+      <div id="header">
+        <slot name="title"></slot>${this.collapsible
+          ? html`
+              <span id="icon"
+                >${this.collapsed ? expand_less : expand_more}</span
+              >
+            `
+          : ""}
+      </div>
+      <div id="body">
+        <slot name="body"></slot>
+      </div>
+      <div id="footer">
+        <slot name="footer"></slot>
+      </div>
     </div>
   `;
 }
