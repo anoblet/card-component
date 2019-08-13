6aca7ea64bfd1fe5f3959960f39b06726586d4f7
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-13 11:09:36 -0400

    Chore: Save

--- src/Style.ts
+++ src/Style.ts
@@ -1,4 +1,20 @@
 import { css } from "lit-element";
 
 export default css`
+  :host {
+    display: flex;
+    flex: 1;
+  }
+
+  #card {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    padding: 1rem;
+  }
+
+  #body {
+    display: flex;
+    flex: 1;
+  }
 `;
